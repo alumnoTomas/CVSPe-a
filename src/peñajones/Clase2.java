9cf9aa16b54126b089972211c79df75daa3a0712
dam <dam@203P02>
2024-03-07 09:46:02 +0100

    Segundo Commit

---
+++ src/peñajones/Clase2.java
@@ -0,0 +1,18 @@
+package peñajones;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dam
+ */
+public class Clase2 {
+    public Clase2(){
+        System.out.println("Estamos en la rama producción.");
+        System.out.println("Estoy en un examen");
+    }
+    
+}
