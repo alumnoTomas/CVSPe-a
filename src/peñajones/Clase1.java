d66eb9a40c376744ffc1def622353f19fd12c291
dam <dam@203P02>
2024-03-07 09:36:30 +0100

    Primer Commit

---
+++ src/peñajones/Clase1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package peñajones;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+    String nombre;
+    String apellido;
+
+    public Clase1(String nombre, String apellido) {
+        this.nombre = "Tomas";
+        this.apellido = "Peña";
+    }
+    
+    
+    
+    
+}
